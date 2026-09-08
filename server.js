a13393e65b827e3f41aa8570cfd920a8e30230c4
gurralavarun <gurrala.varun11@gmail.com>
2026-09-08 12:05:53 +0530

    added

--- server.js
+++ server.js
@@ -18,7 +18,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
